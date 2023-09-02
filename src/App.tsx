235a70eb2a4a35f2b55c384c3635fa08a328c825
Derrick Maina <mburumaina648@gmail.com>
2023-09-02 21:39:25 +0300

    added the showcase section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./Components/Layout/Header"
+import Showcase from "./Components/Layout/Showcase"
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Showcase/>
 
     </div>
   )
