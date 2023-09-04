afca4abdd91ee7df5697516a15c4f9ef45ff8fd0
Derrick Maina <mburumaina648@gmail.com>
2023-09-04 22:04:19 +0300

    added carousel to the testimonial section

--- src/Components/Layout/CTA.tsx
+++ src/Components/Layout/CTA.tsx
@@ -2,7 +2,7 @@ const CTA = () => {
   return (
     <div className="bg-BrightRed text-white  py-12 mt-32">
       <div className="max-w-[1300px] mx-auto">
-         <div className="flex flex-col items-center justify-between space-y-6 md:flex-row md:space-y-0">
+         <div className="flex flex-col px-4 items-center justify-between space-y-6 md:flex-row md:space-y-0">
             <h1 className="font-bold text-3xl max-w-sm">
                Simplify how your team works today.
             </h1>
--- src/Components/Layout/Testimonail.tsx
+++ src/Components/Layout/Testimonail.tsx
@@ -1,39 +1,56 @@
-import data from '../../db.json'
+import data from '../../db.json';
+import { Carousel } from '@mantine/carousel';
+import { useState, useEffect } from 'react';
 
+const Testimonial = () => {
+  
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
 
-const Testimonail = () => {
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+ 
+ 
+
   return (
-    
     <div className="flex flex-col text-center space-y-4 mt-10">
       <h1 className="font-bold text-3xl text-DarkBlue">What they've said</h1>
 
-      <div className='flex flex-col space-y-10   px-4 items-center justify-center md:flex-row md:space-x-20 md:px-0'>
-     
-          {data.users.map((user)=>{
-         return(
-            <div key={user.id} className='flex flex-col space-y-4 items-center justify-center'>
-               <img src={user.img} alt="" className='w-24' />
-               <p className='text-xs'>{user.name}</p>
-               <p className='text-DarkGrayishBlue text-sm'>{user.Testimonial}</p>
-
-
-            </div>
-         )
-      })}
-      
-
-
-      </div>
+      <Carousel
+        withIndicators
+        height={400}        
+        slideSize={isSmallScreen ? '100%' : '33.333333%'}
+        slideGap="md"
+        loop
+        align="start"
+       
+       
+      >
+        {data.users.map((user) => {
+          return (
+            <Carousel.Slide key={user.id} className="flex flex-col space-y-4 items-center justify-center ">
+              <img src={user.img} alt="" className="w-24" />
+              <p className="text-xs">{user.name}</p>
+              <p className="text-DarkGrayishBlue text-sm max-w-md">{user.Testimonial}</p>
+            </Carousel.Slide>
+          );
+        })}
+      </Carousel>
 
       <div>
-        <button  className=' bg-BrightRed text-white text-sm p-3 rounded-full'>
-            Get Started
-         </button>
-      </div>     
-
-
-     
+        <button className="bg-BrightRed text-white text-sm p-3 rounded-full">Get Started</button>
+      </div>
     </div>
-  )
-}
-export default Testimonail
+  );
+};
+
+export default Testimonial;
