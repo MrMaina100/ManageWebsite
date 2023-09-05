6fb26366f459c5dca1a0fb10dfebfc42967586a9
Derrick Maina <mburumaina648@gmail.com>
2023-09-05 19:49:24 +0300

    fixed bugs that was causing production problems

--- src/Components/Layout/Testimonail.tsx
+++ src/Components/Layout/Testimonail.tsx
@@ -1,7 +1,10 @@
-import data from '../../db.json';
+
 import { Carousel } from '@mantine/carousel';
 import { useState, useEffect } from 'react';
-
+import avatarAli from '../../images/avatar-ali.png'
+import avartarAnisha from '../../images/avatar-anisha.png'
+import avatarRichard from '../../images/avatar-richard.png'
+import avatarShanai from '../../images/avatar-shanai.png'
 const Testimonial = () => {
   
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
@@ -35,15 +38,30 @@ const Testimonial = () => {
        
        
       >
-        {data.users.map((user) => {
-          return (
-            <Carousel.Slide key={user.id} className="flex flex-col space-y-4 items-center justify-center ">
-              <img src={user.img} alt="" className="w-24" />
-              <p className="text-xs">{user.name}</p>
-              <p className="text-DarkGrayishBlue text-sm max-w-md">{user.Testimonial}</p>
+        
+        
+            <Carousel.Slide className="flex flex-col space-y-4 items-center justify-center ">
+              <img src={avartarAnisha} alt="" className="w-24" />
+              <p className="text-xs">Anisha Li</p>
+              <p className="text-DarkGrayishBlue text-sm max-w-md">Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.</p>
             </Carousel.Slide>
-          );
-        })}
+            <Carousel.Slide className="flex flex-col space-y-4 items-center justify-center ">
+              <img src={avatarAli} alt="" className="w-24" />
+              <p className="text-xs">Trisha Takanawa</p>
+              <p className="text-DarkGrayishBlue text-sm max-w-md">We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.</p>
+            </Carousel.Slide>
+             <Carousel.Slide className="flex flex-col space-y-4 items-center justify-center ">
+              <img src={avatarRichard} alt="" className="w-24" />
+              <p className="text-xs">Richard </p>
+              <p className="text-DarkGrayishBlue text-sm max-w-md">Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!</p>
+            </Carousel.Slide>
+             <Carousel.Slide className="flex flex-col space-y-4 items-center justify-center ">
+              <img src={avatarShanai} alt="" className="w-24" />
+              <p className="text-xs">Shania chan </p>
+              <p className="text-DarkGrayishBlue text-sm max-w-md">Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.</p>
+            </Carousel.Slide>
+          
+       
       </Carousel>
 
       <div>
